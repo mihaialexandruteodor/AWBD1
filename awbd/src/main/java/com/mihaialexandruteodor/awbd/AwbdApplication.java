66f3ddfc9675734d671153f7b2b2c42cc6e0633e
mihaiteodor97 <mihaialexandruteodor@gmail.com>
2021-03-18 09:58:36 +0200

    *added spring io boilerplate

---
+++ awbd/src/main/java/com/mihaialexandruteodor/awbd/AwbdApplication.java
@@ -0,0 +1,13 @@
+package com.mihaialexandruteodor.awbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwbdApplication.class, args);
+	}
+
+}
---
+++ awbd/src/test/java/com/mihaialexandruteodor/awbd/AwbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihaialexandruteodor.awbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
