f729ccfbe24f1ba8d6c1ec94145f338ec160e2cb
mihaiteodor97 <mihaialexandruteodor@gmail.com>
2021-03-25 12:15:52 +0200

    * related to last

--- laboratoare/SURSE_FINAL/LAB7/src/main/java/com/awbd/lab7/ProiectAWBD.java
+++ laboratoare/SURSE_FINAL/LAB7/src/main/java/com/awbd/lab7/ProiectAWBD.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab7Application {
+public class ProiectAWBD {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab7Application.class, args);
+		SpringApplication.run(ProiectAWBD.class, args);
 	}
 
 }
