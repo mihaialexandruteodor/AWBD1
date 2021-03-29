651c8b705d4115f5a37db3c58608133899087cd3
mihaiteodor97 <mihaialexandruteodor@gmail.com>
2021-03-29 18:04:54 +0300

    * related to last

--- awbd/src/main/java/com/awbd/proiect/domain/Movie.java
+++ awbd/src/main/java/com/awbd/proiect/domain/Movie.java
@@ -1,2 +1,38 @@
-package com.awbd.proiect.domain;public class Movie {
+package com.awbd.proiect.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int release_year;
+    private Long price;
+
+    @Min(value=100, message ="min price 100")
+    private Double reservePrice;
+
+    private Boolean restored;
+
+    @OneToOne(mappedBy = "product",
+            cascade = CascadeType.ALL, orphanRemoval = true)
+    private Info info;
+
+    @ManyToMany(
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "movie_category",
+            joinColumns = @JoinColumn(name = "movie_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id",
+                    referencedColumnName = "id"))
+    private List<Category> categories;
 }
--- awbd/src/main/resources/import.sql
+++ awbd/src/main/resources/import.sql
@@ -5,3 +5,5 @@
 --insert into product (name, code, reserve_price) values ('The Card Players', 'PCEZ', 250);
 
 --insert into product_category values(1,1);
+
+
